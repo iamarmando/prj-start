a0cea3b3c0651fd3e884507ccf158f6a60bb5a91
armando <armando.azarcon@gmail.com>
2018-11-04 01:05:33 -0800

    auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDmKiHuUq-GRI90lRSDQsS1d26NvSseKrs",
+      authDomain: "ng-recipe-book-c156f.firebaseapp.com"
+      });
+  }
 
   onNavigate(feature: string) {
     this.loadedFeature = feature;
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,22 +3,26 @@ import { Http, Response } from "@angular/http";
 import 'rxjs/Rx';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable()
 export class DataStorageService {
   url = "https://ng-recipe-book-c156f.firebaseio.com/recipes.json";
   constructor(
     private http: Http,
-    private recipeService: RecipeService) {}
+    private recipeService: RecipeService,
+    private authService: AuthService) {}
 
   storeRecipes() {
+    const token = this.authService.getIdToken();
     return this.http.put(
-      this.url, 
+      this.url + "?auth=" + token, 
       this.recipeService.getRecipes())
   }
 
   getRecipes() {
-    this.http.get(this.url)
+    const token = this.authService.getIdToken();
+    this.http.get(this.url + "?auth=" + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
